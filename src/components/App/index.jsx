678d3d3095bdc4e74e4a36294ae81292e0e71317
GromovRV <gromovrv@lad24.ru>
2020-07-17 05:05:48 +0300

    added some styles and close modal Click Outside

--- src/components/App/index.jsx
+++ src/components/App/index.jsx
@@ -4,13 +4,13 @@ import { YMaps, withYMaps } from "react-yandex-maps";
 import { Modal } from "../Modal";
 import { DeliveryAddressForm } from "../DeliveryAddressForm";
 
-import styles from './index.scss';
+import styles from './index.module.scss';
 
 function App() {
   const [isAddress, setAddress] = useState(false);
   const [isModal, setIsModal] = useState(false);
   const hocWithYMaps = withYMaps(DeliveryAddressForm, true);
-
+  
   return (
     <div className={styles.app}>
       <div className="content">
@@ -27,15 +27,13 @@ function App() {
           </button>
         )}
       </div>
-      <div className="modal">
-        {isModal && (
-          <Modal onClose={() => setIsModal(false)}>
-            <YMaps>
-              {hocWithYMaps()}
-            </YMaps>
-          </Modal>
-        )}
-      </div>
+      {isModal && (
+        <Modal onClose={() => setIsModal(false)}>
+          <YMaps>
+            {hocWithYMaps()}
+          </YMaps>
+        </Modal>
+      )}
     </div>
   );
 }
--- src/components/Modal/index.jsx
+++ src/components/Modal/index.jsx
@@ -1,13 +1,30 @@
-import React from "react";
-import { createPortal } from "react-dom";
+import React, { useRef }  from "react";
+import { createPortal, findDOMNode } from "react-dom";
+
+import styles from "./index.module.scss";
 
 const modalRoot = document.getElementById("modal");
 
 export const Modal = ({ onClose, children }) => {
+  const ref = useRef();
+
+  const handleClickOutside = (event) =>  {
+    const domNode = findDOMNode(ref.current);
+
+    if ((!domNode || !domNode.contains(event.target))) {
+      onClose();
+    }
+  }
   const content = () => (
-    <div className={'modal'}>
-      <div onClick={onClose}>X</div>
-      {children}
+    <div  className={styles.modal} >
+      <div  className={styles.content} onClick={handleClickOutside}>
+        <div className={styles.close}>
+          X
+        </div>
+        <div ref={ref}>
+          {children}
+        </div>
+      </div>
     </div>
   );
   return createPortal(content(), modalRoot);
