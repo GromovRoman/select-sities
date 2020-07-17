b38e29f1888a2ec2cd2922f635b73904e9de75e5
GromovRV <gromovrv@lad24.ru>
2020-07-17 04:24:09 +0300

    added module node-sass

--- src/components/App/index.jsx
+++ src/components/App/index.jsx
@@ -4,13 +4,15 @@ import { YMaps, withYMaps } from "react-yandex-maps";
 import { Modal } from "../Modal";
 import { DeliveryAddressForm } from "../DeliveryAddressForm";
 
+import styles from './index.scss';
+
 function App() {
   const [isAddress, setAddress] = useState(false);
   const [isModal, setIsModal] = useState(false);
   const hocWithYMaps = withYMaps(DeliveryAddressForm, true);
 
   return (
-    <div className="app">
+    <div className={styles.app}>
       <div className="content">
         {isAddress ? (
           <input type={"text"} />
